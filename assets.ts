69fcd2f848da61221e6cb905e6e6d99b7446a847
shakao-test <61802990+shakao-test@users.noreply.github.com>
2020-07-30 12:07:31 -0700

    Initial files for MakeCode project

---
+++ assets.ts
@@ -0,0 +1,7 @@
+namespace spritepackSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const one = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const two = image.ofBuffer(hex``);
+
+}
